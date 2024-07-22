894f36d02f43e9abc78514d305c7abcfecc27810
ImJaeOne <dlawi0108@naver.com>
2024-07-22 21:35:05 +0900

    타입 가드

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,25 @@
-// 할당 단언(Assertion)
+//타입 가드(Guards)
 
-let num!: number;
-console.log(num);
+function logText(el: Element) {
+    console.log(el.textContent);
+}
+
+const h1El = document.querySelector('h1');
+// logText(h1El)
+if (h1El instanceof HTMLHeadingElement) {
+    // null
+    logText(h1El);
+}
+
+function add(val: string | number) {
+    let res = 'Result =>';
+    if (typeof val === 'number') {
+        res += val.toFixed(2);
+    } else {
+        res += val.toUpperCase();
+    }
+    console.log(res);
+}
+
+add(3.141592);
+add('hello world');
