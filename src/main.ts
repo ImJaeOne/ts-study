550857094d52b4ac7906f80eeb4c0d4d82a91bbb
ImJaeOne <dlawi0108@naver.com>
2024-07-22 21:14:59 +0900

    할당 단언

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,4 @@
-// Non-null 단언 연산자 - !
+// 할당 단언(Assertion)
 
-// 1) const el = HTMLBodyElement | null
-const el = document.querySelector('body');
-el!.textContent = 'Hello world?!';
-
-// 2) x: number | null | undefined
-function getNumber(x: number | null | undefined) {
-    return Number(x!.toFixed(2));
-}
-getNumber(3.1415926535);
-getNumber(null);
-
-// 3)
-function getValue(x: string | number, isNumber: boolean) {
-    if (isNumber) {
-        return Number((x as number).toFixed(2));
-    }
-    return (x as string).toUpperCase();
-}
-getValue('hello world', false);
-getValue(3.14159926535, true);
+let num!: number;
+console.log(num);
