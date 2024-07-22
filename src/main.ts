396bf7c1c624892eec3ec03d03f35df871f0cd9c
ImJaeOne <dlawi0108@naver.com>
2024-07-22 21:08:25 +0900

    타입 단언 - !

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
-// 타입 단언(Assertion)
-// '단언' - 주저하지 아니하고 딱 잘라 말함
-
-// 단언 키워드 - as
 // Non-null 단언 연산자 - !
 
 // 1) const el = HTMLBodyElement | null
-const el = document.querySelector('body') as HTMLBodyElement;
-el.textContent = 'Hello world?!';
+const el = document.querySelector('body');
+el!.textContent = 'Hello world?!';
 
 // 2) x: number | null | undefined
 function getNumber(x: number | null | undefined) {
-    return Number((x as number).toFixed(2));
+    return Number(x!.toFixed(2));
 }
 getNumber(3.1415926535);
 getNumber(null);
